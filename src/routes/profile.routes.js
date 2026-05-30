78944efef9abc470347a2ebd4f01736ca4dc0fba
Subhransu-Rout <subhranshurout68@gmail,com>
2026-05-30 21:53:53 +0530

    added default route

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const profileController = require('../controllers/profile.controller');
 const { route } = require('../app');
 
+router.get('/',(req, res)=>{
+    res.status(200).json({
+        message: "API is live"
+    })
+})
+
 router.post('/profiles', profileController.analyzeAndStoreProfile);
 
 router.get('/profiles', profileController.getAllProfiles);
